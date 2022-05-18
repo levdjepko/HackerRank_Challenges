1d78f5e2bbca361c7a2f3552ba3bf90baf7d77d9
Lev <levdjepko@gmail.com>
2022-05-18 14:13:40 -0700

    Create differenceBetweenTheDiagonalsInSquareArray.java
    Array is a square array, return the absolute difference between its diagonals

---
+++ differenceBetweenTheDiagonalsInSquareArray.java
@@ -0,0 +1,28 @@
+class differenceBetweenTheDiagonalsInSquareArray {
+
+    /*
+     * Complete the 'diagonalDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+11 2 4
+4 5 6
+10 8 -12
+     */
+
+    public static int diagonalDifference(List<List<Integer>> arr) {
+    // Write your code here
+        int size = arr.size();
+        int firstDiagonal = 0;
+        int secondDiagonal = 0;
+        
+        // get the first diagonal items - same i and j, essentially
+        for (int i = 0; i < size; i++) {
+            firstDiagonal += arr.get(i).get(i);
+            secondDiagonal += arr.get(i).get(size - i - 1);
+        } 
+        int difference = Math.abs(firstDiagonal - secondDiagonal);
+        return difference;
+    }
+
+}
