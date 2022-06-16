d9c15949aa013a9d807ff3a0f91a37af3f88fdca
Lev <levdjepko@gmail.com>
2022-06-16 11:07:45 -0700

    Create gradingStudents.java

---
+++ gradingStudents.java
@@ -0,0 +1,16 @@
+public static List<Integer> gradingStudents(List<Integer> grades) {
+    // Write your code here
+        List<Integer> result = new ArrayList<Integer>();
+        for (Integer grade : grades) {
+            if (grade < 38) {
+                result.add(grade);
+            } else if ( grade % 5 > 2){
+               int gradeIntPortion = grade / 5;
+               grade = gradeIntPortion * 5 + 5;
+               result.add(grade);
+            } else {
+                result.add(grade);
+            }
+        }
+        return result;
+    }
