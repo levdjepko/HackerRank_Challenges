188b67c72dd025644abafb0ff3203287efc38f2c
Lev <levdjepko@gmail.com>
2022-06-17 08:04:46 -0700

    Update differenceBetweenTheDiagonalsInSquareArray.java

--- differenceBetweenTheDiagonalsInSquareArray.java
+++ differenceBetweenTheDiagonalsInSquareArray.java
@@ -24,5 +24,16 @@ class differenceBetweenTheDiagonalsInSquareArray {
         int difference = Math.abs(firstDiagonal - secondDiagonal);
         return difference;
     }
+    /*public static int diagonalDifference(List<List<Integer>> arr) {
+    // Write your code here
+        int sumDiagonal1 = 0;
+        int sumDiagonal2 = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            sumDiagonal1 += arr.get(i).get(i);
+            sumDiagonal2 += arr.get(i).get(arr.size() - 1 - i);
+        }
+        int difference = Math.abs(sumDiagonal1 - sumDiagonal2);
+        return difference;
+    }*/
 
 }
