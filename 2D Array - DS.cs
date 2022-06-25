ccc17cdb451745a9257edfcdb052f6d4e4ea7f58
Lev <levdjepko@gmail.com>
2022-06-24 19:46:41 -0700

    Update 2D Array - DS.cs

--- 2D Array - DS.cs
+++ 2D Array - DS.cs
@@ -16,8 +16,7 @@ class Result
 {
 
     /*
-     * Complete the 'hourglassSum' function below.
-     *
+     * Complete the 'hourglassSum' function below.     
      * The function is expected to return an INTEGER.
      * The function accepts 2D_INTEGER_ARRAY arr as parameter.
      */
@@ -30,7 +29,7 @@ class Result
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 tempSum =           arr[i][j]   + arr[i][j+1] + arr[i][j+2];
-                tempSum =               tempSum + arr[i+1][j+1];
+                tempSum =           tempSum + arr[i+1][j+1];
                 tempSum = tempSum + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
                 if ( tempSum >= max) {
                     max = tempSum;
