e43d497eca5eec62ed205ef2c7ace10fdb149626
Lev <levdjepko@gmail.com>
2022-07-05 08:42:14 -0700

    Create maximumPerimeterTriangle.java

---
+++ maximumPerimeterTriangle.java
@@ -0,0 +1,26 @@
+class Result {
+
+    /*
+     * Complete the 'maximumPerimeterTriangle' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY sticks as parameter.
+     */
+
+    public static List<Integer> maximumPerimeterTriangle(List<Integer> sticks) {
+        Collections.sort(sticks);
+        int side1, side2, side3;
+        for (int i = sticks.size() - 1; i >= 2; i--) {
+            side1 = sticks.get(i);      // longest side
+            side2 = sticks.get(i - 1);  // 2nd longest
+            side3 =  sticks.get(i - 2); // shortest side
+            if (side2 + side3 > side1){
+                List<Integer> resultingTriangle = new ArrayList<Integer>(Arrays.asList(side3,side2,side1));
+                return resultingTriangle;
+            }
+        }
+        return new ArrayList<Integer>(Arrays.asList(-1));
+
+    }
+
+}
