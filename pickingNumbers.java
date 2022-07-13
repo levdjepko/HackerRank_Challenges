33d4a4d0a2fabbf8da7cda7069b5f330296cd92f
Lev <levdjepko@gmail.com>
2022-07-12 20:35:52 -0700

    Create pickingNumbers.java

---
+++ pickingNumbers.java
@@ -0,0 +1,23 @@
+public static int pickingNumbers(List<Integer> a) 
+    {
+    	
+        HashMap<Integer, Integer> frequency = new HashMap<>();  
+      
+        for (int i = 0; i < a.size(); ++i) 
+        {  
+            if(frequency.containsKey(a.get(i)))  
+                frequency.put(a.get(i), freq.get(a.get(i)) + 1);  
+            else
+                frequency.put(a.get(i), 1);  
+        }  
+      
+        // Finding max sum of adjacent indices  
+        int ans = 0;  
+      
+        for (Integer key : freq.keySet())  
+        {  
+            if(frequency.containsKey(key+1))  
+                ans = Math.max(ans, frequency.get(key) + frequency.get(key+1));  
+        }  
+        return ans;  
+    }  
