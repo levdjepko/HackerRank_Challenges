ecde7d58cd8f1369d7818b908726c8b7d193c898
Lev <levdjepko@gmail.com>
2022-07-18 19:09:30 -0700

    Create separateTheNumbers.java

---
+++ separateTheNumbers.java
@@ -0,0 +1,19 @@
+ public static void separateNumbers(String s)  {
+    // Write your code here
+
+        String sub = "";
+        boolean isValid = false;
+        for (int i = 1; i <= s.length() / 2; i++) {
+            sub = s.substring(0, i);
+            long num = Long.parseLong(sub);
+            StringBuilder valid = new StringBuilder(sub);
+            while (valid.length() < s.length()) {
+                valid.append(++num);
+            }
+            if (s.equals(valid.toString())) {
+                isValid = true;
+                break;
+            }
+        }
+        System.out.println(isValid ? "YES " + sub : "NO");
+    }
