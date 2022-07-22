0539888ebb17833108eeb251dcf4a303e3df0dfb
Lev <levdjepko@gmail.com>
2022-07-22 07:44:51 -0700

    Create maxMin.java

---
+++ maxMin.java
@@ -0,0 +1,24 @@
+public static int maxMin(int k, List<Integer> arr) {
+        // create an array of length k with 
+        //smallest difference between MAX elemenet and MIN element
+        Collections.sort(arr);
+        // we implement a sliding window and check Max 
+        // and Min while iterating over all the elements of the input array
+        int left = 0;
+        int right = k - 1;
+        int currentMax = arr.get(right);
+        int currentMin = arr.get(0);
+        
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr.size() > i + k - 1) {
+                int max = arr.get(i + k - 1);
+                int min = arr.get(i);
+                if (max - min < currentMax - currentMin) {
+                    currentMax = max;
+                    currentMin = min;
+                }
+            }
+        }
+        return currentMax - currentMin;
+
+    }
