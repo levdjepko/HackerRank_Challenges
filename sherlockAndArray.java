398a260dc6ce7752270f9928102998faf621a7c2
Lev <levdjepko@gmail.com>
2022-08-03 20:46:50 -0700

    Update sherlockAndArray.java

--- sherlockAndArray.java
+++ sherlockAndArray.java
@@ -1,33 +1,18 @@
-// THIS QUESTION IS FAULTY, as well as the answer. DON'T USE IT
+
 public static String balancedSums(List<Integer> arr) {
         // find an element in the array such that sum of elements to the left equals sum of elements to the right
         int left = 0;
-        int right = arr.size() - 1;
-        
-        int leftSum = arr.get(0);
-        int rightSum = arr.get(arr.size() - 1);
-        while (right - left > 1) {
-
-            if (leftSum == rightSum && right - left == 2) {
-                return "YES";
-            }
-            
-            if (leftSum < rightSum) {
-                left++;
-                leftSum = leftSum + arr.get(left);
-                continue;
-            }
-            if (rightSum < leftSum) {
-                right--;
-                rightSum += arr.get(right);
-                continue;
-            }
-
+        int right = 0;
+        for (int i = 1; i < arr.size(); i++) {
+            right += arr.get(i);
         }
-        if (leftSum == rightSum && right - left == 2) {
+        for (int i = 0; i < arr.size() - 1; i++) {
+            if (left == right) {
                 return "YES";
-            } else {
-                return "NO";
             }
+            left += arr.get(i);
+            right -= arr.get(i + 1);
+        }
+        return "NO";
 
     }
