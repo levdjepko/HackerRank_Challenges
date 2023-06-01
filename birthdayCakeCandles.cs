db7aac30276e3052a88da1a21a22251c07c4ab6d
Lev <levdjepko@gmail.com>
2023-06-01 15:34:56 -0700

    Create birthdayCakeCandles.cs

---
+++ birthdayCakeCandles.cs
@@ -0,0 +1,13 @@
+public static int birthdayCakeCandles(List<int> candles)
+    {
+        candles.Sort();
+        int count = 1;
+        int item = candles[candles.Count - 1];
+        for (int i = candles.Count - 2; i> 0; i--) {
+            if (candles[i] != item) {
+                break;
+            }
+            count++;
+        }
+        return count;
+    }
