48b69a357b7e4d581968ab720f1c513fe4c0158b
Lev <levdjepko@gmail.com>
2025-06-04 09:30:23 -0700

    Update pairsInArray.java

--- pairsInArray.java
+++ pairsInArray.java
@@ -8,14 +8,6 @@ import java.util.regex.*;
 
 class Result {
 
-    /*
-     * Complete the 'sockMerchant' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts following parameters:
-     *  1. INTEGER n
-     *  2. INTEGER_ARRAY ar
-     */
 
     public static int sockMerchant(int n, List<Integer> ar) {
         HashMap <Integer, Integer> pairs = new HashMap<>();
